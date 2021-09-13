7301449683997e7e426ec12be0758c76125b014d
saurabhd10 <saurabhdharamkar2714@gmail.com>
2021-09-13 12:24:59 -0700

    change

--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public class Sum{
 		
 		int num1 = 5, num2 = 10, sum;
 		sum = num1 + num2;
-		System.out.println("Sum of these numbers: "+ sum);
+		System.out.println("Sum of these numbers is: "+ sum);
 	}
 }
